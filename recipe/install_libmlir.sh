b52c2f949a96ed4ee2ce8abf468716e188281787
Daniel Petry <dpetry@anaconda.com>
2023-12-14 20:16:45 -0600

    On unix, copy in libraries to prefix rather than deleting all libs in prefix

--- recipe/install_libmlir.sh
+++ recipe/install_libmlir.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -x -e
 
-cd ${SRC_DIR}/build
-ninja install
+# cd ${SRC_DIR}/build
+# ninja install
 
-cd $PREFIX
-rm -rf libexec share bin include
-mv lib lib2
-mkdir lib
-cp lib2/libLLVM* lib/
-if [[ "$PKG_NAME" == "libmlir" ]]; then
-    cp lib2/libMLIR${SHLIB_EXT} lib/
-    cp lib2/libmlir_runner_utils${SHLIB_EXT} lib/
-    cp lib2/libmlir_c_runner_utils${SHLIB_EXT} lib/
-    cp lib2/libmlir_async_runtime${SHLIB_EXT} lib/
-    cp lib2/libmlir_float16_utils${SHLIB_EXT} lib/
+# cd $PREFIX
+# rm -rf libexec share bin include
+# mv lib lib2
+# mkdir lib
+# mv lib2/libLLVM* lib/
+mkdir temp_prefix
+cmake --install ./build --prefix=./temp_prefix
+if [[ "$PKG_NAME" == "libmlir" ]]; then   
+    mv ./temp_prefix/libMLIR${SHLIB_EXT} $PREFIX/lib
+    mv ./temp_prefix/libmlir_runner_utils${SHLIB_EXT} $PREFIX/lib
+    mv ./temp_prefix/libmlir_c_runner_utils${SHLIB_EXT} $PREFIX/lib
+    mv ./temp_prefix/libmlir_async_runtime${SHLIB_EXT} $PREFIX/lib
+    mv ./temp_prefix/libmlir_float16_utils${SHLIB_EXT} $PREFIX/lib
 else
-    cp lib2/libMLIR.*.* lib/
-    cp lib2/libmlir_runner_utils.*.* lib/
-    cp lib2/libmlir_c_runner_utils.*.* lib/
-    cp lib2/libmlir_async_runtime.*.* lib/
-    cp lib2/libmlir_float16_utils.*.* lib/
+    mv ./temp_prefix/libMLIR.*.* $PREFIX/lib
+    mv ./temp_prefix/libmlir_runner_utils.*.* $PREFIX/lib
+    mv ./temp_prefix/libmlir_c_runner_utils.*.* $PREFIX/lib
+    mv ./temp_prefix/libmlir_async_runtime.*.* $PREFIX/lib
+    mv ./temp_prefix/libmlir_float16_utils.*.* $PREFIX/lib
 fi
-rm -rf lib2
+rm -rf temp_prefix
 
