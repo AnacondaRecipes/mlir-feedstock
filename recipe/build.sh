fc4c3c3ba2b75781513c674d966e4861a268ab0a
Daniel Petry <dpetry@anaconda.com>
2024-01-23 18:15:54 -0600

    Remove nonexistent tool from end of list

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,7 +40,7 @@ cmake --build . -- -j${CPU_COUNT}
 
 # the helper tools used by lit are expected to be in ${PREFIX}/bin. Perhaps they should be put here while building llvm,
 # or maybe we can use LIT_OPTS or a CMake argument to do this better, see https://llvm.org/docs/CommandGuide/lit.html
-tools=(count FileCheck lli-child-target llvm-jitlink-executor llvm-PerfectShuffle not obj2yaml split-file UnicodeNameMappingGenerator yaml2obj yaml-bench bin)
+tools=(count FileCheck lli-child-target llvm-jitlink-executor llvm-PerfectShuffle not obj2yaml split-file UnicodeNameMappingGenerator yaml2obj yaml-bench)
 for tool in "${tools[@]}"; do
     cp "${PREFIX}/libexec/llvm/${tool}" "${PREFIX}/bin/"
 done
